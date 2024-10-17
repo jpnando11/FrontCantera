2cb8224264240820bea8167441b8b165962351b0
jpmacias <jpmacias@cobiscorp.com>
2024-10-17 15:34:53 -0500

    ajustes

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import axiosRetry from "axios-retry";
 
 const api = axios.create({
-    baseURL: "http://localhost:4002/",
+    baseURL: "http://localhost:3000/",
     timeout: 10000,
 })
 
